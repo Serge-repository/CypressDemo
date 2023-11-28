021567e00e1d578c83bf3745d4739c547eebd4b8
Serge-repository <sderii802@gmail.com>
2023-11-28 18:22:23 +0200

    reporting tool integrated

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "8y3o33",
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: true,
   videoCompression: false,
   defaultCommandTimeout: 5000,
   pageLoadTimeout: 20000,
+  reporter: 'cypress-mochawesome-reporter',
   retries: {
     runMode: 1,
     openMode: 1
@@ -15,7 +17,7 @@ module.exports = defineConfig({
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
--- cypress/e2e/FirstTest.spec.js
+++ cypress/e2e/FirstTest.spec.js
@@ -5,5 +5,15 @@ describe("My first test suite", () => {
         cy.visit("https://magento.softwaretestingboard.com")
         cy.get("#search").type("Radiant Tee")
         cy.get("#search_autocomplete li").should("have.length", 3)
+        cy.get("strong.product-item-name").as("productsSelector")
+        cy.get("@productsSelector").find("a").should("have.length", 6)
+    })
+
+    it("failed test", () => {
+        cy.visit("https://magento.softwaretestingboard.com")
+        cy.get("#search").type("Radiant Tee")
+        cy.get("#search_autocomplete li").should("have.length", 3)
+        cy.get("strong.product-item-name").as("productsSelector")
+        cy.get("@productsSelector").find("a").should("have.length", 2)
     })
 })
