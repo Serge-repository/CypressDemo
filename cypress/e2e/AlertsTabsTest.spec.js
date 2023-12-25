d5718596edff295cae9970be4ad31daaf3217627
Serge-repository <sderii802@gmail.com>
2023-12-25 12:01:07 +0200

    alerts tabs

---
+++ cypress/e2e/AlertsTabsTest.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+describe("Alerts and tabs suite", () => {
+    it("Alerts test", () => {
+        cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
+        cy.get("button[onclick='jsAlert()']").click()
+        cy.on("window:alert", (text) => {
+            expect(text).to.eq("I am a JS Alert")
+        })
+        cy.get("button[onclick='jsConfirm()']").click()
+        cy.on("window:confirm", (text) => {
+            expect(text).to.include("JS Confirm")
+        })
+    })
+
+    it("Alerts test", () => {
+        cy.visit("https://the-internet.herokuapp.com/windows")
+        cy.get("a[href='/windows/new']").invoke("removeAttr", "target").click()
+        cy.contains("New Window").should("be.visible")
+        cy.go("back")
+        cy.go("forward")
+        cy.contains("New Window").should("be.visible")
+    })
+
+})
